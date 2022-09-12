8c99ab1034372e00fe25ef8a2202c10de8d08114
Héctor Robles <robletes062901@gmail.com>
2022-09-12 14:16:34 -0500

    csv python dict parser

---
+++ python/get_instrucions_set.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+
+def get_instrucions_set():
+    data = dict()
+    file = open("../docs/op_codes_dictionarie.csv", "r")
+    for instruction in file:
+        instructions = instruction.split(",")
+        data[instructions[0]] = instructions[1:-1]
+    return data
+
+if __name__ == "__main__":
+    get_instrucions_set()
---
+++ python/main.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+
+from get_instrucions_set import *
+
+def main():
+    data = get_instrucions_set()
+    instruction = input("Instruction: ")
+    if instruction in data:
+        print(data[instruction])
+
+if __name__ == "__main__":
+    main()
